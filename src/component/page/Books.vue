1215aef2699bbd9228fdfcfd4cefdad5ab7a6dfd
gaconkzk <gaconkzk@gmail.com>
2017-09-30 12:15:09 +0700

    starting include firebase

---
+++ src/component/page/Books.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+#manga
+  table
+    thead: tr
+      th Title
+      th Author
+    tbody: tr(v-for='book in books')
+      td: a {{book.name}}
+      td {{book.author}}
+</template>
+
+<script>
+import Firebase from 'firebase'
+
+// Initialize Firebase
+let config = {
+  apiKey: 'AIzaSyA_HE_wTvXEr3YojYi8DbzVP4pVsez2L_o',
+  authDomain: 'tkg-vue.firebaseapp.com',
+  databaseURL: 'https://tkg-vue.firebaseio.com',
+  projectId: 'tkg-vue',
+  storageBucket: 'tkg-vue.appspot.com',
+  messagingSenderId: '645087845803'
+}
+let app = Firebase.initializeApp(config)
+
+let db = app.database()
+let booksRef = db.ref('books')
+
+export default {
+  name: 'tkg-books',
+  firebase: {
+    books: booksRef
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
