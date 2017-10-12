83a6e9c86be35974326d114431b1bb1b5ef9a747
To Nguyen Thanh Vu <tntvu@TMA.COM.VN>
2017-10-12 18:26:23 +0700

    bulma for header

--- src/component/common/Header.vue
+++ src/component/common/Header.vue
@@ -1,26 +1,43 @@
 <template lang="pug">
-header.alt(id='header')
-	h6
-		a(href='/') TKG
-		|
-		| by The Flies
-	nav#nav: ul
-		li: a(href='/') gác
-		li
-			a.icon.fa-angle-down(href='#') xem sách
-			ul
-				li: a(href='/manga') hình
-				li: a(href='/book') chữ
-		li
-			a.icon.fa-angle-down(href='#') ăn chơi
-			ul
-				li: a(href='/drinking') ăn nhậu
-				li: a(href='/games') chơi bời
-				li: a(href='/girls') gái gú
-		li
-			a(href='/login') đăng nhập
-			| hay
-			a.button(href='/register') đăng ký
+header#header.alt
+  h1
+    a(href='/') TKG
+    |
+    | by The Flies
+  nav#nav
+    .navbar-end
+      a(href='#', slot='trigger') 
+        span gác
+      b-dropdown(hoverable)
+        a(href='#', slot='trigger')
+          b-icon(icon='arrow_drop_down')
+          span xem sách
+        b-dropdown-item.sub: a(href='/manga') hình
+        b-dropdown-item.sub: a(href='/book') chữ
+      b-dropdown(hoverable)
+        a(href='#', slot='trigger')
+          b-icon(icon='arrow_drop_down')
+          span ăn chơi
+        b-dropdown-item.sub: a(href='/drinking') ăn nhậu
+        b-dropdown-item.sub: a(href='/game') chơi bời
+        b-dropdown-item.sub: a(href='/girls') gái gú
+  //- nav#nav: ul
+  //- 	li: a(href='/') gác
+  //- 	li
+  //- 		a.icon.fa-angle-down(href='#') xem sách
+  //- 		ul
+  //- 			li: a(href='/manga') hình
+  //- 			li: a(href='/book') chữ
+  //- 	li
+  //- 		a.icon.fa-angle-down(href='#') ăn chơi
+  //- 		ul
+  //- 			li: a(href='/drinking') ăn nhậu
+  //- 			li: a(href='/games') chơi bời
+  //- 			li: a(href='/girls') gái gú
+  //- 	li
+  //- 		a(href='/login') đăng nhập
+  //- 		| hay
+  //- 		a.button(href='/register') đăng ký
 </template>
 
 <script>
@@ -31,19 +48,31 @@ export default {
 
 <style lang="scss" scoped>
 #header h6 {
-	color: inherit;
-	height: inherit;
-	left: 1.25em;
-	line-height: inherit;
-	margin: 0;
-	padding: 0;
-	position: absolute;
-	top: 0;
+  color: inherit;
+  height: inherit;
+  left: 1.25em;
+  line-height: inherit;
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  top: 0;
 }
-#header h6 a {
-	color: #fff;
-	font-weight: 400;
-	border: 0;
+
+#header h1 a {
+  color: #fff;
+  font-weight: 400;
+  border: 0;
+}
+
+#header a {
+  border-bottom: none;
+  color: #fff;
+  font-weight: 400;
+  border: 0;
+}
+
+#header .sub a {
+  color: #000;
 }
 </style>
 
