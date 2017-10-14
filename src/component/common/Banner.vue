1e0dada2f1527d8a6d1ae09a38c1b01d6f764275
gaconkzk <gaconkzk@gmail.com>
2017-10-14 07:07:15 +0700

    link

--- src/component/common/Banner.vue
+++ src/component/common/Banner.vue
@@ -3,7 +3,7 @@
     <h2>Tàng Kinh Gác</h2>
     <blockquote>"Xa chốn ngựa xe xuân mộng đến<br/>Vui bên sách vở tuổi già ngâm"<cite> - Nguyễn Bỉnh Khiêm</cite></blockquote>
     <ul class="actions">
-      <li><a href="#" class="html5up-button special">Đăng ký</a></li>
+      <li><a href="/register" class="html5up-button special">Đăng ký</a></li>
       <li><a href="#" class="html5up-button">Tìm hiểu thêm</a></li>
     </ul>
   </section>
