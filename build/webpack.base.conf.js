fb0945e2f2e448336f8573e86040959401737ea8
To Nguyen Thanh Vu <tntvu@TMA.COM.VN>
2017-11-06 17:43:01 +0700

    seem that I need to impl the drawing myself

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -73,7 +73,11 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
-      }
+      },
+      {
+        test: /fabric(\.min)?\.js$/,
+        use: 'exports-loader?fabric'
+      },
     ]
   }
 }
---
+++ src/component/common/PCanvas.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  canvas(:width='width', :height='height', ref='canvas')
+</template>
+
+<script>
+export default {
+  props: ['width', 'height'],
+  data() {
+    return {
+      canvas: {}
+    }
+  },
+  mounted() {
+    this.canvas = this.$refs.canvas
+    this.context = this.canvas.getContext('2d')
+    this.context.rect(20, 20, 150, 100)
+    this.context.stroke()
+
+    this.context.fillStyle = '#FFFFFF'
+    this.context.fill() // (20, 20, 150, 100)
+  }
+}
+</script>
+
